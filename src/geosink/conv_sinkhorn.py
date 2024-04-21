4eb0b9202ccc29e038ef4e40e7e0cad79039140b
Guillaume Huguet <57917099+guillaumehu@users.noreply.github.com>
2024-04-21 16:20:36 -0400

    fmt

--- src/geosink/conv_sinkhorn.py
+++ src/geosink/conv_sinkhorn.py
@@ -1,14 +1,30 @@
-from geosink.heat_kernel import HeatFilter
 import numpy as np
+
+from geosink.heat_kernel import HeatFilter
+
 
 class ConvSinkhorn:
     def __init__(self, tau, order, method, graph=None, lap=None):
-        assert (graph is not None) or (lap is not None), "Either graph or lap must be provided."
+        assert (graph is not None) or (
+            lap is not None
+        ), "Either graph or lap must be provided."
         self.tau = tau
         kwargs_filter = {"lap": lap} if graph is None else {"graph": graph}
-        self.heat_filter = HeatFilter(tau = tau, order = order, method= method,**kwargs_filter)
+        self.heat_filter = HeatFilter(
+            tau=tau, order=order, method=method, **kwargs_filter
+        )
 
-    def __call__(self, m_0, m_1, stopThr=1e-4, max_iter=1e3, verbose=False, return_vw=False, stopping_crit=False, stopping_fn=lambda x: False):
+    def __call__(
+        self,
+        m_0,
+        m_1,
+        stopThr=1e-4,
+        max_iter=1e3,
+        verbose=False,
+        return_vw=False,
+        stopping_crit=False,
+        stopping_fn=lambda x: False,
+    ):
         eps = 1e-8
         assert m_0.shape == m_1.shape, "m_0 and m_1 must have the same shape"
         N = m_0.shape[0]
@@ -20,7 +36,12 @@ class ConvSinkhorn:
             w_prev = w
             v = m_0 / (self.heat_filter(a * w) + eps)
             w = m_1 / (self.heat_filter(a * v) + eps)
-            if np.any(np.isnan(v)) or np.any(np.isnan(w)) or np.any(np.isinf(v)) or np.any(np.isinf(w)):
+            if (
+                np.any(np.isnan(v))
+                or np.any(np.isnan(w))
+                or np.any(np.isinf(v))
+                or np.any(np.isinf(w))
+            ):
                 v = v_prev
                 w = w_prev
                 break
@@ -32,11 +53,18 @@ class ConvSinkhorn:
                         print("converged at iteration %d" % i)
                     break
             if stopping_crit:
-                kl = np.sum(4 * self.tau * a * (m_0 * np.log(v + eps) + m_1 * np.log(w + eps)))
+                kl = np.sum(
+                    4
+                    * self.tau
+                    * a
+                    * (m_0 * np.log(v + eps) + m_1 * np.log(w + eps))
+                )
                 if stopping_fn(kl):
                     print("converged at iteration %d" % i)
                     break
-        kl = np.sum(4 * self.tau * a * (m_0 * np.log(v + eps) + m_1 * np.log(w + eps)))
+        kl = np.sum(
+            4 * self.tau * a * (m_0 * np.log(v + eps) + m_1 * np.log(w + eps))
+        )
         if return_vw:
             return kl, v, w
         return kl
